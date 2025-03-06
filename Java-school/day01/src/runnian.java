25d5956ec0b30afaa9e1878e07d7d6a2394a2a53
zxh <lncode7581@AtomGit.com>
2025-03-06 14:00:04 +0800

    more

--- Java-school/day01/src/runnian.java
+++ Java-school/day01/src/runnian.java
@@ -1,30 +1,28 @@
 public class runnian {
     public static void main(String[] args) {
        test1();
-        System.out.println("-------------------------------------------------");
+        System.out.println("\n-------------------------------------------------");
        test2();
     }
-    //判断1950-2050年中的闰年（两种方法）
     public static void test1() {
         for (int i = 1950; i <= 2050; i++) {
             if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
                 System.out.print(i + "是闰年\t");
             }
-            if(i % 16 == 0)
+            if((i+4) % 16 == 0)
                 System.out.println();
         }
     }
     public static void test2(){
-        int i;
-        for (i = 1950; i <= 2050; i++){
-            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0){
-                break;
+        for (int i = 1950; i <= 2050; i++) {
+            if (isRunNian(i)) {
+                System.out.print(i + "是闰年\t");
             }
-        }
-        for(;i<=2050;i+=4){
-            System.out.print(i + "是闰年\t");
-            if(i % 16 == 0)
+            if((i+4) % 16 == 0)
                 System.out.println();
         }
+    }
+    public static boolean isRunNian(int year){
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 }
